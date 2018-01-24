790ec02378d683ca6cf5b089d1a54b2a557fec7e
andrewphillipdoss <andrewphillipdoss@gmail.com>
2018-01-23 18:23:59 -0600

    edited sandwich eligibility

--- app/models.py
+++ app/models.py
@@ -17,11 +17,14 @@ class Person(models.Model):
     stop_time = models.TimeField(blank=True, null=True)
 
     def calculateSandwiches(self):
-        unique = self.unique_time.hour*60*60*1000 + self.unique_time.minute*60*1000 + self.unique_time.second*1000 + self.unique_time.microsecond*1000
-        stop = self.stop_time.hour*60*60*1000 + self.stop_time.minute*60*1000 + self.stop_time.second*1000 + self.stop_time.microsecond*1000
-        milli_diff = (stop - unique)
-        sandwich_num = max(0, (milli_diff - ((60*6 + 12)*60*1000))/(1000*60*30))
-        self.sandwiches = math.ceil(sandwich_num)
+        if self.eligible == False:
+            self.sandwiches = None
+        else:
+            unique = self.unique_time.hour*60*60*1000 + self.unique_time.minute*60*1000 + self.unique_time.second*1000 + self.unique_time.microsecond*1000
+            stop = self.stop_time.hour*60*60*1000 + self.stop_time.minute*60*1000 + self.stop_time.second*1000 + self.stop_time.microsecond*1000
+            milli_diff = (stop - unique)
+            sandwich_num = max(0, (milli_diff - ((60*6 + 12)*60*1000))/(1000*60*30))
+            self.sandwiches = math.ceil(sandwich_num)
 
 
     def __unicode__(self):
