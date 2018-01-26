6abab0e4ffc8f42501ad30e5ec82f511a70258e5
andrewphillipdoss <andrewphillipdoss@gmail.com>
2018-01-26 14:27:58 -0600

    test

--- grace/settings.py
+++ grace/settings.py
@@ -15,7 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
