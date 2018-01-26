825e6b963ff8eeb9a8634e80d515cc68a4ebb717
andrewphillipdoss <andrewphillipdoss@gmail.com>
2018-01-26 14:31:36 -0600

    test

--- grace/settings.py
+++ grace/settings.py
@@ -20,7 +20,7 @@ STATIC_URL = '/static
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
