f20a75381e1e84a5097e39a51c8944452d8568cf
andrewphillipdoss <andrewphillipdoss@gmail.com>
2018-01-26 13:31:37 -0600

    updated STATIC_ROOT

--- grace/settings.py
+++ grace/settings.py
@@ -15,7 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, '/app/static/')
 STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
@@ -107,5 +107,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
